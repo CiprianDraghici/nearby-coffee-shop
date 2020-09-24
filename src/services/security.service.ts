4ce9516f077ef9c49ca59e38e54d715ef649db54
cipriand <ciprian.draghici@webfactory-world.de>
2020-09-24 15:48:32 +0300

    refactor security.service and add unit tests

--- src/services/security.service.ts
+++ src/services/security.service.ts
@@ -1,11 +1,6 @@
 export class SecurityService {
     public getToken() {
         let token = sessionStorage.accessToken || localStorage.accessToken;
-
-        if(token && token !== "undefined") {
-            return token;
-        } else {
-            return null;
-        }
+        return token || null;
     }
 }
